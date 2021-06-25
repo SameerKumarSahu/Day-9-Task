d9ad82c7bd377ea306ac2a110daaa75d261d50fc
SameerKumarSahu <sameerkumarsahu0@gmail.com>
2021-06-26 00:22:00 +0530

    Emp Wage using Function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,25 +4,32 @@ echo "Welcome Message"
 
 isPartTime=1;
 isFullTime=2;
-totalSalary=0;
-empRatePerHr=20;
+maxHrsInMonth=100;
+empRatePerHrs=20;
 numWorkingDays=20;
 
-for (( day=1; day<=$numWorkingDays; day++ ))
+totalWorkingHours=0;
+totalWorkingDays=0;
+
+function getWorkingHours() {
+        case $1 in
+                $isFullTime)
+                        workHours=8
+                        ;;
+                $isPartTime)
+                        workHours=4
+                        ;;
+                *)
+                        workHours=0
+                        ;;
+        esac
+}
+while [[ $totalWOrkingHours -lt $maxHrsInMonth &&
+        $totalWorkingHours -lt $numWorkingDays ]]
 do
-        empCheck=$((RANDOM%3));
-                case $empCheck in
-                        $isFullTime)
-                                empHrs=8
-                                ;;
-                        $isPartTime)
-                                empHrs=8
-                                ;;
-                        *)
-                                empHrs=0
-                                ;;
-                esac
-                salary=$(($empHrs*$empRatePerHr));
-                totalSalary=$(($totalSalary+$salary));
+        ((totalWorkingDays++))
+        getWorkingHours $((RANDOM%3))
+        totalWorkingHours=$(($totalWorkingHours+$workHours));
 done
 
+totalsalary=$(($totalWorkingHours*$empRatePerHrs));
